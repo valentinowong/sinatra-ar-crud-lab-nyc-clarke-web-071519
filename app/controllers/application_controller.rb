d27372c576d696bdec081930aeb4ac99032eb4e5
Valentino Wong <valentino.wong@gmail.com>
2019-08-06 23:55:13 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,44 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.all.find(params[:id])
+    erb :show
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.all.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.all.find(params[:id])
+    @article.update(title: params[:title],content: params[:content])
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.all.find(params[:id])
+    @article.destroy
+
+    redirect to "/articles"
+  end
+
 end
